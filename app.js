f7648bf7f57654dfb718c72d658854ca086bfd0d
AbdullahBinNasir <a.dshamsi2207@gmail.com>
2024-01-26 15:54:45 +0500

    Added Array Methods In Js File

---
+++ app.js
@@ -0,0 +1,129 @@
+//Task 1 (Pop Methoad)
+// var fruit = ["Apple", "Mango", "Orange", "Grapes"]
+// var fruit_pop = fruit.pop()
+// console.log(fruit)
+
+//Task 2 (Push Methoad)
+// var num = [1, 2, 3, 4, 5, 6]
+// var num_push = num.push(7)
+// console.log(num)
+
+// Task 3 (Shift Methoad)
+// var color = ["Blue", "Black", "Purple", "Orange"]
+// var color_shift = color.shift()
+// console.log(color)
+
+// Task 4 (UnShift Methoad)
+// var language = ["Python", "Java", "C++", "C."]
+// var language_unshift = language.unshift("JavaScript")
+// console.log(language)
+
+// Task 5 (Slice Methoad)
+// var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+// var days_slice = days.slice(0, 5)
+// console.log(days_slice)
+
+//Task 6 (Reverse Methoad)
+// var characters = ['R', 'E', 'V', 'E', 'R', 'S', 'E']
+// var characters_reverse = characters.reverse()
+// console.log(characters_reverse)
+
+//Task 7 (Concat Methoad)
+// var fruits = ["Apple", "Mango", "Orange", "Grapes"]
+// var vegetables = ["Potato", "Eggplant", "Tomato", "Cucumber"]
+// var concating = fruits.concat(vegetables)
+// console.log(concating)
+
+//Task 8 (Join Methoad)
+// var word = ["A", "Quick", "Black", "Brown", "Fox"]
+// var word_join = word.join(" ")
+// console.log(word_join)
+
+//Task 9 (Splice Methoad)
+// var numb = [1, 2, 3, 4, 5, 6]
+// var numb_splice = numb.splice(1, 3)
+// console.log(numb_splice)
+
+// Task 10 (Map Methoad)
+// var numbs = [1, 2, 3, 4, 5, 6]
+// var numb_map = numbs.map((double) => double * 2);
+// console.log(numb_map)
+
+// Task 11 (Filter Methoad)
+// var age = [12, 15, 18, 20, 22, 23]
+// var age_filter = age.filter((abc) => abc > 21);
+// console.log(age_filter)
+
+// Task 12 (Find Method)
+// var book = [
+//     {
+//         Book: "abc",
+//         page: 250
+//     },
+//     {
+//         Book: "bcd",
+//         page: 300
+//     },
+//     {
+//         Book: "cde",
+//         page: 400
+//     },
+//     {
+//         Book: "def",
+//         page: 200
+//     }
+
+// ]
+
+// var book_find = book.find((book) => book.page >= 300)
+// console.log(book_find)
+
+// Task 13 (Every Methoad)
+
+// var ages = [20, 30, 40, 60, 80, 100]
+// var ages_every = ages.every((age) => age > 60)
+// console.log(ages_every)
+
+// Task 14 (Some Methoad)
+
+// var temp = [20, 30, 40, 60, 80, 100]
+// var temp_some = temp.some((temp) => temp > 30)
+// console.log(temp_some)
+
+// Task 15 (ForEach Methoad)
+
+// var names = ["Abdullah", "Huzayfah", "Ahmed", "Ali"]
+// var names_foreach = names.forEach(function (names) {
+//     console.log("Hello " +  names)
+// })
+
+// console.log(names_foreach)
+
+// Task 16 (Include Methoad)
+// var cities = ["Karachi", "Lahore", "Islamabad", "Peshawar"]
+// var cities_include = cities.includes("Karachi")
+// console.log(cities_include)
+
+// Task 17 (Reduce Methoad)
+// var numbers = [100,200,300,400,500,600]
+// var numbers_reduce = numbers.reduce((sum,total) => {
+//     return sum + total  
+// }  )
+// console.log(numbers_reduce)
+
+// Task 19 (Index-Of Methoad )
+// var colors = ["Blue", "Black", "Orange", "Purple"]
+// var colors_index = colors.indexOf("Purple")
+// console.log(colors_index)
+
+// Task 20 (Last-IndexOf Methoad)
+
+// var values = [20, 30, 20, 50, 40, 60, 20]
+// var values_lastindex = values.lastIndexOf(20)
+// console.log(values_lastindex)
+
+// Task 21 (To-String Methoad)
+
+// var mixed = [20, "Ali", true, 60, "Ahmed", false]
+// var mixed_tostring = mixed.toString()
+// console.log(typeof (mixed_tostring))
